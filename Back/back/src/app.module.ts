a30af21b6ab380b9d6abff83b939f51ac9d24c51
Francisco Arenas <franciscoarenas.2094@gmail.com>
2026-03-05 14:02:46 -0400

    Use environment variable for database port

--- Back/back/src/app.module.ts
+++ Back/back/src/app.module.ts
@@ -15,7 +15,7 @@ import { Category } from './products/entities/category.entity';
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: process.env.DB_HOST,
-      port: 5440,
+      port: process.env.DB_PORT,
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
